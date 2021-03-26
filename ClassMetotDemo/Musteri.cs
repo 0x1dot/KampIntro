3c9e5cac7ec806743fd754c618d3c70da870feeb
Murat Dursun <41049765+0x1dot@users.noreply.github.com>
2021-03-26 16:35:04 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public long TC { get; set; }
+        public long TelNo { get; set; }
+        public string Adres { get; set; }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Bilgisayar");
+            myDictionary.Add(2, "Mouse");
+            myDictionary.Add(3, "Klavye");
+            myDictionary.Add(4, "LCD Ekran");
+            myDictionary.Add(5, "Usb Flash");
+        }
+    }
+}
